5507dcb108168fa330a70244e8abbc64df5a3472
shvjubayer-source <shvjubayer@gmail.com>
2025-12-30 17:18:01 +0600

    added one binary search problem

---
+++ binary_search/C_Coloring_Game.cpp
@@ -0,0 +1,60 @@
+//https://codeforces.com/contest/2112/problem/C
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i, st, ed) for(int i=st; i<=ed; i++)
+#define ll long long
+#define watch(x) cout<<(#x)<<" = " <<(x)<<endl;
+#define test int t; cin>>t; while(t--)
+#define gcin(s) getline(cin, s);
+#define vi vector<int>
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define vv(name, type, rows, cols) vector<vector<type>> name(rows, vector<type>(cols))
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define edge(v) g[v].size()
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define nl '\n'
+#define pvec(a) do{for(auto &x:a) cout<<x<<" "; cout<<endl;}while(0)
+//bit manipulation
+#define checkbit(x,n) (x&(1LL<<n))
+#define setbit(x,n) (x=(x|(1LL<<n)))
+#define unsetbit(x,n) (x=(x&(~(1LL<<n))))
+#define togglebit(x,n) (x=(x^(1LL<<n)))
+
+const ll M = 1000000007;
+
+
+
+void solve(){
+    int n;
+    cin>>n;
+    vi a(n);
+    f(i, 0, n-1) cin>>a[i];
+
+    ll ans=0;
+
+
+    f(i, 0, n-3){
+        f(j, i+1, n-2){
+
+            auto l=upper_bound(a.begin()+j+1, a.end(), a[n-1]-a[i]-a[j]);
+            auto r=lower_bound(a.begin()+j+1, a.end(), a[i]+a[j]);
+
+            if(r==a.begin()+j) continue;
+            r--;
+            ans+=max(0LL, r-l+1);
+
+        }
+    }
+
+    cout<<ans<<nl;
+
+}
+
+int main(){
+    fastio;
+    test solve();
+    return 0;
+}
