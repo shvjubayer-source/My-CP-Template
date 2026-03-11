f764e42d4887fac648f8a85346c677a6a81e7373
shvjubayer-source <shvjubayer@gmail.com>
2026-03-12 00:35:58 +0600

    adfdafas

--- DSU/DSU_template.cpp
+++ DSU/DSU_template.cpp
@@ -3,37 +3,38 @@ using namespace std;
 
 class DSU {
 private:
-    vector<int> parent, rank;
+    vector<int> parent, size;
 
 public:
     DSU(int n) {
         parent.resize(n);
-        rank.resize(n, 0);
-        for(int i=0; i<n; i++)parent[i]=i;
+        size.resize(n, 1);
+
+        for(int i = 0; i < n; i++)
+            parent[i] = i;
     }
 
-
-    int find(int x){
-        if(parent[x]!=x) parent[x]=find(parent[x]);
+    int find(int x) {
+        if(parent[x] != x)
+            parent[x] = find(parent[x]); 
 
         return parent[x];
     }
 
+    bool unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
 
-    bool unite(int x, int y){
-        int px=find(x);
-        int py=find(y);
+        if(px == py) return false;
 
-        if(px==py) return false;
-
-        if(rank[px]<rank[py]){
-            parent[px]=py;
-        }else if(rank[px]>rank[py]){
-            parent[py]=px;
-        }else{
-            parent[py]=px;
-            rank[px]++;
+        if(size[px] < size[py]) {
+            parent[px] = py;
+            size[py] += size[px];
+        } else {
+            parent[py] = px;
+            size[px] += size[py];
         }
+
         return true;
     }
 };
